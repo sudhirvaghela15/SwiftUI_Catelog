e75ff6f9704f3bd1f2f00caa9d2fb25fad7af413
sudhir <sudhirvaghela15@hotmail.com>
2025-01-16 23:15:53 +0530

    first commit

---
+++ SwiftUICatelog/Modules/Controlls/PickerComponentsView.swift
@@ -0,0 +1,18 @@
+//
+//  PickerComponentsView.swift
+//  SwiftUICatelog
+//
+//  Created by sudhir on 14/01/25.
+//
+
+import SwiftUI
+
+struct PickerComponentsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PickerComponentsView()
+}
---
+++ SwiftUICatelog/Modules/Controlls/SteppersComponentsView.swift
@@ -0,0 +1,18 @@
+//
+//  SteppersComponentsView.swift
+//  SwiftUICatelog
+//
+//  Created by sudhir on 14/01/25.
+//
+
+import SwiftUI
+
+struct SteppersComponentsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SteppersComponentsView()
+}
